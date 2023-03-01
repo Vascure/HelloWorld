bb3c16e65e5ab9087662276ce678eb21725daad9
Troppenauer Jana Carina <s53037@edu.campus02.at>
2023-03-01 15:57:38 +0100

    Typical Hello World

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("I really like cheese, you know");
+
+        System.out.println(12 + 78);
+
+        System.out.println("Die Summe ist:" + (13*8) );
+
+        System.out.println(14 == 19);
+
+    // Erster Versuch eines kleinen Programms um 9:11
+    /* Dies ist ein Blockkommentar
+    So kann ich mehrere Zeilen machen
+    siehst du?Easy!*/
+    }
+
+}
